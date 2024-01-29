de7b4196a880bd053066db51f4e1126e89949e0c
Koby Fink <finkkoby@icloud.com>
2024-01-29 17:30:37 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    if (pickup > 42) {
+        return pickup - 42;
+    } else if (pickup < 42) {
+        return 42 - pickup;
+    } else {
+        return 0;
+    }
+}
+function distanceFromHqInFeet(pickup) {
+    let blocks = distanceFromHqInBlocks(pickup);
+    return blocks * 264;
+}
+function distanceTravelledInFeet(pickup, dropoff) {
+    let feet = pickup >= dropoff ? pickup - dropoff : dropoff - pickup;
+    return feet * 264;
+}
+function calculatesFarePrice(pickup, dropoff) {
+    let distance = distanceTravelledInFeet(pickup, dropoff);
+    if (distance < 400) {
+        return 0;
+    } else {
+        if (distance <= 2000) {
+            return (distance - 400) * .02;
+        } else { 
+            if (distance <= 2500) {
+                return 25;
+            } else {
+                return "cannot travel that far";
+            }
+        }
+    }
+}
